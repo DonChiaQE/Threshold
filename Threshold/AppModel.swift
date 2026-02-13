266d04f153ed31a7f3a7a6b75f410fdce523b34d
lizardon <24926784+DonChiaQE@users.noreply.github.com>
2026-02-13 14:31:21 +0800

    Initial Commit

---
+++ Threshold/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Threshold
+//
+//  Created by Don Chia on 13/2/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Threshold/ThresholdApp.swift
@@ -0,0 +1,33 @@
+//
+//  ThresholdApp.swift
+//  Threshold
+//
+//  Created by Don Chia on 13/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ThresholdApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
